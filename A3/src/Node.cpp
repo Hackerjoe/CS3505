91609f695997ac6362d0a1f29a32c31cd9ffd0b7
hackerjoe <jspankin5@gmail.com>
2016-02-02 15:13:23 -0700

    Node Completed

--- A3/src/Node.cpp
+++ A3/src/Node.cpp
@@ -1,1 +1,59 @@
 #include "Node.h"
+
+Node::Node()
+{
+
+}
+
+Node::~Node()
+{
+	for (std::vector< Node* >::iterator it = children.begin() ; it != children.end(); ++it)
+    {
+		delete (*it);
+    }
+	children.clear(); 
+}
+
+bool Node::isEndOfWord()
+{
+	return word;
+}
+
+void Node::addChildNode(Node* node)
+{
+	children.push_back(node);
+}
+
+void Node::SetValue(char value)
+{
+	Value = value;
+}
+
+void Node::SetMarkEnd()
+{
+	word = true;
+}
+
+char Node::GetValue()
+{
+	return Value;
+}
+
+Node* Node::GetChild(char value)
+{
+	for(int i = 0; i < children.size(); i++)
+	{
+		Node* CurrentChild = children.at(i);
+		if(CurrentChild->GetValue() == value)
+		{
+			return CurrentChild;
+		}
+	}
+	
+	return NULL;
+}
+
+int Node::GetLength()
+{
+	return children.size();
+}
--- A3/src/Node.h
+++ A3/src/Node.h
@@ -4,14 +4,20 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 class Node 
 {
 public:
+	Node();
+	~Node();
 	bool isEndOfWord();
-	void addChild(char value)
+	void addChildNode(Node* node);
 	void SetMarkEnd();
 	void SetValue(char value);
 	Node* GetChild(char value);
+	char GetValue();
+	int GetLength();
 private:
 	char Value;
 	bool word;
